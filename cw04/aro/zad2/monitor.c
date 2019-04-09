4357101bd763693fed2862c7b3c6634413ec7cea
Mateusz Naróg <narogmateusz@gmail.com>
2019-04-09 18:38:38 +0200

    small fixes

--- cw04/aro/zad2/monitor.c
+++
@@ -1,5 +0,0 @@
-//
-// Created by mateusz on 03.04.19.
-//
-
-#include "monitor.h"
--- cw04/aro/zad3/common.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by mateusz on 03.04.19.
-//
-
-#ifndef ARO_COMMON_H
-#define ARO_COMMON_H
-
-#endif //ARO_COMMON_H
